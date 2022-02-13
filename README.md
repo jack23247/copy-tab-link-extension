89acf0e160a0e54fc1d10105741088dbed05bd4a
Devin Boyer <drb5272@gmail.com>
2022-02-13 16:30:54 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# copy-tab-link-extension
+
+Adds an item to a tab's context menu in Firefox to copy the tab's URL to the clipboard.
---
+++ background.js
@@ -0,0 +1,49 @@
+/*
+Called when the item has been created, or when creation failed due to an error.
+We'll just log success/failure here.
+*/
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+
+/**
+ * Copies text to the clipboard
+ * Based on this browser extension code: https://github.com/mdn/webextensions-examples/blob/master/context-menu-copy-link-with-types/clipboard-helper.js
+ * @param {string} text the text to copy to the clipboard
+ */
+function copyToClipboard(text) {
+  function onCopy(event) {
+    document.removeEventListener("copy", onCopy, true);
+    // Hide the event from the page to prevent tampering.
+    // TODO: not sure what this means
+    event.stopImmediatePropagation();
+
+    // set clipboard content
+    event.preventDefault();
+    event.clipboardData.setData("text/plain", text);
+  }
+  document.addEventListener("copy", onCopy, true);
+
+  // this requires the clipboardWrite permission
+  document.execCommand("copy");
+}
+
+browser.menus.create({
+  id: "copy-tab-url",
+  title: "Copy Tab URL",
+  contexts: ["tab"]
+}, onCreated);
+
+/*
+ * Click event listener
+*/
+browser.menus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "copy-tab-url") {
+    copyToClipboard(tab.url);
+  }
+});
